b38a8b63eb35e8ce9295fd7f55b9c23e533f066a
Jose Castillo <jose@onfocusdev.com>
2020-08-28 12:28:12 -0300

    se agrega webkitdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const refInput = useRef();
+  useEffect(() => {
+    refInput.current.webkitdirectory = true;
+    refInput.current.directory = true;
+    refInput.current.multiple = true;
+    console.log("refInput.current.directory", refInput.current.directory)
+    console.log("refInput.current.webkitDirectory", refInput.current.webkitDirectory)
+    },[])
+
+const [rootFolder, setRootFolder] = useState('');
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-      
         <p>
           Extraer metadata de carpetas
-        </p>
-<input type="file" />
+       
+      </p>
+      <p style={{fontSize:15}}>
+Seleccione la carpeta raiz donde se contienen las carpetas con los archivos fotograficos
+          </p>
+    {rootFolder?  (  <p>
+         Carpeta raiz: {rootFolder}
+        </p>):null}
 
-
+        <input ref={refInput} onChange={(e)=> console.log("log: ", e.target.files) } type='file' />
       </header>
     </div>
   );
