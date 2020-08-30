40269776aefcb551c52fce59779874219d933696
Jose Castillo <jose@onfocusdev.com>
2020-08-30 00:05:49 -0300

    se extrae metada de imagenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
-import fs from 'browserify-fs';
+
+import * as ExifReader from 'exifreader';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import './App.css';
@@ -18,13 +19,36 @@ function App() {
     refInput.current.webkitdirectory = true;
     refInput.current.directory = true;
     refInput.current.multiple = true;
+    if (!supportsFileReader()) {
+      alert('Sorry, your web browser does not support the FileReader API.');
+      return;
+    }
     },[])
 
+
+let listFiles =  [];
 const [rootFolder, setRootFolder] = useState('');
 const [rootFolderFoldersNumber, setRootFolderNumberFolders] = useState(0);
 const [rootFolderFilesNumber, setRootFolderFilesNumber] = useState(0);
 const [isLoading, setIsLoading] = useState(false);
-const selectedFolder =(rootFolderFiles)=>{
+const [foldersFiles, setFoldersFile] = useState(null);
+const [metadataFiles, setMetadataFiles] = useState([]);
+
+useEffect(()=> {
+console.log("listFiles useEffect: ", metadataFiles.length -1,
+ "rootFolderFilesNumber: ",rootFolderFoldersNumber)
+
+if ( metadataFiles.length -1 ===  rootFolderFoldersNumber) {
+
+  console.log("completed :", metadataFiles)
+}
+
+
+  }, [listFiles, metadataFiles, metadataFiles.length, rootFolderFoldersNumber])
+
+
+
+const selectedFolder = async(rootFolderFiles)=>{
   setIsLoading(true);
   const rootFolder = rootFolderFiles[0].webkitRelativePath.split('/')[0];
   setRootFolder(rootFolder);
@@ -41,21 +65,70 @@ const foldersFiltered = [...rootFolderFiles].filter((file, index)=>{
 })
 setRootFolderNumberFolders(foldersFiltered.length)
 setIsLoading(false);
-console.log("folders filtered",  foldersFiltered)
-
+///console.log("folders filtered",  foldersFiltered)
+setFoldersFile(foldersFiltered)
 /* se crean archivos de imagenes con nombre de cada carpeta */
-fs.mkdir('/home', function() {
-	fs.writeFile('/home/hello-world.txt', 'Hello world!\n', function() {
-		fs.readFile('/home/hello-world.txt', 'utf-8', function(err, data) {
-			console.log("ok: ", data);
-		});
-	});
-});
 
  return foldersFiltered
+}
+function supportsFileReader() {
+  return window.FileReader !== undefined;
+}
+
+
+const getDataFile =(file)=>{
+  const rootFolder = file.webkitRelativePath.split('/')[0];
+/* mapeando carpetas */
+  const folderName = file.webkitRelativePath.split('/')[1]
+const data = {
+  rootFolder, 
+  folderName,
+  name: file.name}
+ return data;
 
 }
 
+
+const extractMetadataFile = (file, index)=>{
+  const reader = new FileReader();
+  reader.onload = function (readerEvent:{target: {result:any}}) {
+    try {
+        var tags = ExifReader.load(readerEvent.target.result);
+        delete tags['MakerNote'];
+        delete tags['WhiteBalance'];
+       
+  const folderImageData = {
+  fileName: getDataFile(file).name,
+  rootFolder: getDataFile(file).rootFolder,
+  folderName: getDataFile(file).folderName,
+  dateTime: tags.DateTime?.description,
+  latitude:tags.GPSLatitude?.description,
+  longitude:tags.GPSLongitude?.description
+  }
+  //console.log("folderImageData: ",folderImageData)
+  //console.log("metadata file: ", listFiles)
+  listFiles.push(folderImageData)
+  setMetadataFiles( [...listFiles, folderImageData])
+return folderImageData;
+
+
+  } catch (error) {
+      console.log("error: ", error, "file: ", file);
+      //alert("Hubo error, intente mas tarde")
+        // Handle error.
+    }
+  };
+ reader.readAsArrayBuffer(file)
+
+return listFiles;
+}
+
+
+const saveFilesImages = async()=> {
+
+ foldersFiles.forEach(async(file, index)=>extractMetadataFile(file, index))
+
+}
 
   return (
     <div className="App">
@@ -79,20 +152,20 @@ Seleccione la carpeta raiz donde se contienen las carpetas con los archivos foto
  {isLoading?(<div className={classes.root}>
       <LinearProgress />
     </div>): null}
-<div>
+
+    {foldersFiles? ( <div>
+      <p  style={{fontSize:15, color:'green'}}>Se guardara un archivo de imagen por cada una de las {' '}
+         {rootFolderFoldersNumber} carpetas</p>
+<button onClick={()=>saveFilesImages()} >Guardar archivos</button>
+</div>) : null
+
+         }<div>
         <input ref={refInput}
         multiple
-         onChange={(e)=>{ selectedFolder(e.target.files) }}
-          type='file' />
+         onChange={(e)=>{ 
+          selectedFolder(e.target.files) }}
+          type={foldersFiles?'hidden':'file'} />
           </div>
-
-{/*          
-          <ol type="A">
-  <li>Julio</li>
-  <li>Carmen</li>
-  <li>Ignacio</li>
-  <li>Elena</li>
-</ol> */}
       </header>
     </div>
   );
